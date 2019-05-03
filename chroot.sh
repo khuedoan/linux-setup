7e378ee2a2d400d071f3aafa7924619853eadc53
Khue Doan <khuedoan98@gmail.com>
2019-05-03 16:29:30 +0700

    Add chroot.sh
    Auto install when in chroot

---
+++ chroot.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pacman -S efibootmgr intel-ucode networkmanager git gvim zsh
+fallocate -l 32G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+vim /etc/fstab
+echo "# Swap" >> /etc/fstab
+echo "/swapfile none swap defaults 0 0" >> /etc/fstab
+ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
+hwclock --systohc
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen && locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'Precision' > /etc/hostname
+efibootmgr -d /dev/nvme0n1 -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u 'initrd=/intel-ucode.img initrd=/initramfs-linux.img root=/dev/nvme0n1p2 rw quiet' -v
+systemctl enable NetworkManager
+passwd
+useradd -m -G wheel -s /bin/zsh khuedoan
+passwd khuedoan
+visudo
