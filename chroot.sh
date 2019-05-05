9586b1f94439896fb8c41439ba0a8c05eb986a5f
Khuê Đoàn <khuedoan98@gmail.com>
2019-05-05 16:23:50 +0000

    Clean up after install

--- chroot.sh
+++ chroot.sh
@@ -6,7 +6,6 @@ fallocate -l 32G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
-vim /etc/fstab
 echo "# Swap" >> /etc/fstab
 echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 
@@ -15,7 +14,9 @@ hwclock --systohc
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen && locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'Precision' > /etc/hostname
+
 efibootmgr -d /dev/nvme0n1 -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u 'initrd=/intel-ucode.img initrd=/initramfs-linux.img root=/dev/nvme0n1p2 rw quiet' -v
+
 systemctl enable NetworkManager
 echo "Changing root password"
 passwd
@@ -23,3 +24,4 @@ useradd -m -G wheel -s /bin/zsh khuedoan
 echo "Changing user password"
 passwd khuedoan
 visudo
+rm chroot.sh
