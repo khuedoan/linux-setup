d6db8234500dde7dfd25696502b47b32876fa196
Khuê Đoàn <khuedoan98@gmail.com>
2019-05-05 16:34:10 +0000

    Change comment for swapfile in fstab

--- chroot.sh
+++ chroot.sh
@@ -6,7 +6,7 @@ fallocate -l 32G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
-echo "# Swap" >> /etc/fstab
+echo "# swapfile" >> /etc/fstab
 echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 
 ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
