2d3f6dea06f06a12df8288c553d6e76103f75562
Khue Doan <khuedoan98@gmail.com>
2019-05-06 23:59:30 +0700

    Check if command success, delay before format

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-ping -c 3 archlinux.org
-timedatectl set-ntp true
-gdisk /dev/nvme0n1
-cfdisk /dev/nvme0n1
-mkfs.fat -F32 /dev/nvme0n1p1
-mkfs.ext4 /dev/nvme0n1p2
-mount /dev/nvme0n1p2 /mnt
-mkdir /mnt/boot
-mount /dev/nvme0n1p1 /mnt/boot
-vim /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devel
-genfstab -U /mnt >> /mnt/etc/fstab
-curl https://khuedoan.me/archguide/chroot.sh > chroot.sh
-chmod +x chroot.sh
-vim chroot.sh
-cp chroot.sh /mnt
+ping -c 3 archlinux.org &&
+timedatectl set-ntp true &&
+gdisk /dev/nvme0n1 &&
+cfdisk /dev/nvme0n1 &&
+sleep 5 &&
+mkfs.fat -F32 /dev/nvme0n1p1 &&
+mkfs.ext4 /dev/nvme0n1p2 &&
+mount /dev/nvme0n1p2 /mnt &&
+mkdir /mnt/boot &&
+mount /dev/nvme0n1p1 /mnt/boot &&
+vim /etc/pacman.d/mirrorlist &&
+pacstrap /mnt base base-devel &&
+genfstab -U /mnt >> /mnt/etc/fstab &&
+curl https://khuedoan.me/archguide/chroot.sh > chroot.sh &&
+chmod +x chroot.sh &&
+vim chroot.sh &&
+cp chroot.sh /mnt &&
 arch-chroot /mnt /chroot.sh
