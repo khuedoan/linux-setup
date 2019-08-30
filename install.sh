85c27dee6e57802761e4a5b95a18c467dce163be
Khue Doan <khuedoan98@gmail.com>
2019-08-30 21:53:30 +0700

    Add options, fully automated except chroot part

--- install.sh
+++ install.sh
@@ -1,19 +1,54 @@
 #!/bin/sh
 
-timedatectl set-ntp true &&
-sgdisk -Z /dev/nvme0n1 &&
-cfdisk /dev/nvme0n1 &&
-sleep 5 &&
-mkfs.fat -F32 /dev/nvme0n1p1 &&
-mkfs.ext4 /dev/nvme0n1p2 &&
-mount /dev/nvme0n1p2 /mnt &&
-mkdir /mnt/boot &&
-mount /dev/nvme0n1p1 /mnt/boot &&
-vim /etc/pacman.d/mirrorlist &&
-pacstrap /mnt base base-devel &&
-genfstab -U /mnt >> /mnt/etc/fstab &&
-curl https://khuedoan.me/archguide/chroot.sh > chroot.sh &&
-chmod +x chroot.sh &&
-vim chroot.sh &&
-cp chroot.sh /mnt &&
+# Options
+erase_disk=1
+disk='nvme0n1'
+boot_partition='p1'
+root_partition='p2'
+
+# Select the mirrors
+vim /etc/pacman.d/mirrorlist
+
+# Update the system clock
+timedatectl set-ntp true
+
+# Partition the disk
+if [ "$erase_disk" -eq 1 ]; then
+    sgdisk -Z /dev/"$disk"
+    fdisk -u -p /dev/"$disk" << \
+EOF
+g
+n
+
+
++1GiB
+n
+
+
+
+w
+EOF
+else
+    cfdisk /dev/"$disk"
+fi
+lsblk
+sleep 5
+
+# Format the partitions
+mkfs.fat -F32 /dev/"$disk$boot_partition"
+mkfs.ext4 /dev/"$disk$root_partition"
+
+# Mount the file systems
+mount /dev/"$disk$root_partition" /mnt
+mkdir /mnt/boot
+mount /dev/"$disk$boot_partition" /mnt/boot
+
+# Install base packages
+pacstrap /mnt base base-devel
+
+# Configure the system
+genfstab -U /mnt >> /mnt/etc/fstab
+curl https://khuedoan.me/archguide/chroot.sh > chroot.sh
+chmod +x chroot.sh
+cp chroot.sh /mnt
 arch-chroot /mnt /chroot.sh
