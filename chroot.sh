6c70716c7cb540121f06f28f1e14a16486aa36a0
Khue Doan <khuedoan98@gmail.com>
2019-08-31 13:26:50 +0700

    Add comments and automate pacman config

--- chroot.sh
+++ chroot.sh
@@ -1,5 +1,10 @@
+#+-------------------------+
+#| Configuration in chroot |
+#+-------------------------+
+# Essential packages
 pacman --noconfirm -S $ucode networkmanager git gvim zsh
 
+# Swap
 if [ -n "$swap_size" ]; then
      fallocate -l $swap_size /swapfile
      chmod 600 /swapfile
@@ -9,12 +14,17 @@ if [ -n "$swap_size" ]; then
      echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 fi
 
+# Timezone and localization
 ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 hwclock --systohc
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen && locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'Precision' > /etc/hostname
 
+# Network
+systemctl enable NetworkManager
+
+# Boot
 if [ "$boot_loader" = "systemd-boot" ]; then
      bootctl --path=/boot install
      echo 'default arch' > /boot/loader/loader.conf
@@ -25,25 +35,29 @@ if [ "$boot_loader" = "systemd-boot" ]; then
      [ -n "$ucode" ] && echo 'initrd  /intel-ucode.img' >> /boot/loader/entries/arch.conf
      echo 'initrd  /initramfs-linux.img' >> /boot/loader/entries/arch.conf
      echo "options root=$root_partition rw quiet" >> /boot/loader/entries/arch.conf
- elif [ "$boot_loader" = "efistub" ]; then
+elif [ "$boot_loader" = "efistub" ]; then
      [ -n "$ucode" ] && ucode_init="initrd=/$ucode.img"
      efibootmgr -d $disk -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "$ucode_init initrd=/initramfs-linux.img root=$root_partition rw quiet" -v
- fi
+fi
 
+# Silent boot
 sed -i 's/^HOOKS=(base udev/HOOKS=(base systemd/g' /etc/mkinitcpio.conf
 mkinitcpio -p linux
 echo "StandardOutput=null\nStandardError=journal+console" | SYSTEMD_EDITOR="tee -a" systemctl edit --full systemd-fsck-root.service
 
-systemctl enable NetworkManager
+# Pacman
+sed -i 's/^#Color/Color/g' /etc/pacman.conf
+sed -i '/#\[multilib\],/#Include/ s/^#//g' /etc/pacman.conf
+pacman -Syy
+
+# Users
 echo "Changing root password"
 passwd
 useradd -m -G wheel -s /bin/zsh -c "$fullname" "$username"
 echo "Changing user password"
 passwd "$username"
 visudo
-sed -i 's/^#Color/Color/g' /etc/pacman.conf
-vim /etc/pacman.conf
-pacman -Syy
 
+# Cleanup
 rm /chroot.sh
 exit
