1bd2d2ede74b54d670614ea70592742a4080ba25
Khue Doan <khuedoan98@gmail.com>
2019-08-31 10:18:32 +0700

    Adjust comment and move select mirrors to installation section

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# Options
+#+---------+
+#| Options |
+#+---------+
 erase_disk=1
 disk='/dev/nvme0n1'
 boot_partition="${disk}p1"
@@ -16,20 +18,15 @@ bootloader='systemd-boot' # systemd-boot or efistub
 timezone='Asia/Ho_Chi_Minh'
 mirrorlist_generator='https://www.archlinux.org/mirrorlist/?country=SG&country=TH&country=VN&protocol=http&protocol=https&ip_version=4&use_mirror_status=on'
 
+#+------------------+
+#| Pre-installation |
+#+------------------+
 # Update the system clock
 timedatectl set-ntp true
 
-# Update and sorting mirrors
-pacman -Syy
-pacman -S pacman-contrib
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl "$mirrorlist_generator" > /etc/mirrorlist
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
-rankmirrors -n 5 /etc/pacman.d/mirrorlist > /etc/pacman.d/mirrorlist
-vim /etc/pacman.d/mirrorlist
-
 # Partition the disk
 if [ "$erase_disk" -eq 1 ]; then
+    # 1GiB for /boot and the rest for /
     sgdisk -Z "$disk"
     fdisk "$disk" << \
 EOF
@@ -59,10 +56,24 @@ mount "$root_partition" /mnt
 mkdir /mnt/boot
 mount "$boot_partition" /mnt/boot
 
+#+--------------+
+#| Installation |
+#+--------------+
+# Select the mirrors
+pacman -Syy
+pacman -S pacman-contrib
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+curl "$mirrorlist_generator" > /etc/mirrorlist
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+rankmirrors -n 5 /etc/pacman.d/mirrorlist > /etc/pacman.d/mirrorlist
+vim /etc/pacman.d/mirrorlist
+
 # Install base packages
 pacstrap /mnt base base-devel
 
-# Configure the system
+#+----------------------+
+#| Configure the system |
+#+----------------------+
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt << \
 EOF
@@ -117,3 +128,4 @@ sed -i 's/^#Color/Color/g' /etc/pacman.conf
 vim /etc/pacman.conf
 pacman -Syy
 EOF
+echo "Done! Reboot to your new system."
