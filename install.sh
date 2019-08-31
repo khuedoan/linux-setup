b8abd4150df0a01f281f34fd1861125900fdd4da
Khue Doan <khuedoan98@gmail.com>
2019-08-31 11:44:57 +0700

    Revert "Add untested chroot section"
    This reverts commit 0b0664d649e0f199179e05af6bf4ed54b13ccc34.

--- install.sh
+++ install.sh
@@ -77,57 +77,7 @@ pacstrap /mnt base base-devel
 #| Configure the system |
 #+----------------------+
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt << \
-EOF
-pacman --noconfirm -S $ucode networkmanager git gvim zsh
-
-if [ -n "$swap_size" ]; then
-    fallocate -l $swap_size /swapfile
-    chmod 600 /swapfile
-    mkswap /swapfile
-    swapon /swapfile
-    echo "# swapfile" >> /etc/fstab
-    echo "/swapfile none swap defaults 0 0" >> /etc/fstab
-fi
-
-ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
-hwclock --systohc
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen && locale-gen
-echo 'LANG=en_US.UTF-8' > /etc/locale.conf
-echo "$hostname" > /etc/hostname
-
-if [ "$boot_loader" = "systemd-boot" ]; then
-    bootctl --path=/boot install
-    echo 'default arch' > /boot/loader/loader.conf
-    echo 'timeout 0' >> /boot/loader/loader.conf
-    echo 'editor  0' >> /boot/loader/loader.conf
-    echo 'title   Arch Linux' > /boot/loader/entries/arch.conf
-    echo 'linux   /vmlinuz-linux' >> /boot/loader/entries/arch.conf
-    [ -n "$ucode" ] && echo 'initrd  /intel-ucode.img' >> /boot/loader/entries/arch.conf
-    echo 'initrd  /initramfs-linux.img' >> /boot/loader/entries/arch.conf
-    echo "options root=$root_partition rw quiet" >> /boot/loader/entries/arch.conf
-elif [ "$boot_loader" = "efistub" ]; then
-    [ -n "$ucode" ] && ucode_init="initrd=/$ucode.img"
-    efibootmgr -d $disk -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "$ucode_init initrd=/initramfs-linux.img root=$root_partition rw quiet" -v
-fi
-
-sed -i 's/^HOOKS=(base udev/HOOKS=(base systemd/g' /etc/mkinitcpio.conf
-mkinitcpio -p linux
-echo "StandardOutput=null\nStandardError=journal+console" | SYSTEMD_EDITOR="tee -a" systemctl edit --full systemd-fsck-root.service
-
-systemctl enable NetworkManager
-echo "Changing root password"
-passwd
-$rootpasswd
-$rootpasswd
-useradd -m -G wheel -s /bin/zsh -c "$fullname" "$username"
-echo "Changing user password"
-passwd "$username"
-$userpasswd
-$userpasswd
-visudo
-sed -i 's/^#Color/Color/g' /etc/pacman.conf
-vim /etc/pacman.conf
-pacman -Syy
-EOF
-echo "Done! Reboot to your new system."
+curl https://khuedoan.me/archguide/chroot.sh > chroot.sh
+chmod +x chroot.sh
+cp chroot.sh /mnt
+arch-chroot /mnt /chroot.sh
