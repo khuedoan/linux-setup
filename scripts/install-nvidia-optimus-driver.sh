506489903f38445db865ecee70bd4385ef89e1d3
Khue Doan <khuedoan98@gmail.com>
2020-08-21 17:00:54 +0700

    Remove deprecated scripts

--- scripts/install-nvidia-optimus-driver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo pacman --noconfirm --needed -S nvidia lib32-nvidia-utils
-trizen --noconfirm --needed -S optimus-manager
-
-sudo sh -c "echo '## Allow members of group wheel to switch graphics card without a password' >> /etc/sudoers"
-sudo sh -c "echo '%wheel ALL=(ALL) NOPASSWD: /usr/bin/prime-switch' >> /etc/sudoers"
