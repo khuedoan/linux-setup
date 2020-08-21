f849322af83cf44d3941c827afe8f8633987aa9d
Khue Doan <khuedoan98@gmail.com>
2020-08-21 23:25:51 +0700

    Remove deprecated script

--- scripts/create-ssh-key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-email=$(whiptail --inputbox "Enter email for SSH key" 10 20 3>&1 1>&2 2>&3)
-ssh-keygen -t rsa -b 4096 -C "${email}"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-
-dotfiles remote set-url origin git@github.com:khuedoan/dotfiles.git
